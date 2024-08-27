a63b741ca508d5715197cb86412b7f6e75e7401b
CoffeeScholar <76543969+LoongBa@users.noreply.github.com>
2024-08-27 17:08:37 +0800

    Ver1.1: 读取城市名字，调用 API 查询城市编码

---
+++ 2024_暑假练习/编程应用练习/06_获取天气信息/WeatherHistory.py
@@ -0,0 +1,50 @@
+# 获取天气历史信息
+
+import requests
+from datetime import datetime
+import regex as re
+import json
+
+
+# 获取指定城市指定月份的天气信息
+def get_weather_history_monthly(city_name, date_month: datetime):
+    date_month = datetime.now().strftime("%Y%m")
+    url = f"http://tianqihoubao.com/lishi/{city_name}/month/{date_month}.html"
+    response = requests.get(url)
+    html = response.text
+    start = html.find("<table")
+    end = html.find("</table>") + len("</table>")
+    html = html[start:end]
+    html = re.sub("[\s]+", " ", html)
+    html = re.sub("[\r\n]+", "", html)
+    # 解析HTML，获取天气信息
+    # <tr><td><a href="/lishi/kunming/20110101.html" title="2011年01月01日昆明天气预报">2011年01月01日</a></td><td>晴/多云</td><td>7℃/17℃</td><td>无持续风向 微风/无持续风向 微风</td></tr>
+    weather_info = {"city_id": city_name, "city_name": "", "history": []}
+    pattern = re.compile(r'<td>.*?<a(.*?)>(?P<date>.*?)</a>.*?</td>.*?<td>(?P<weather>.*?)</td>.*?<td>(?P<temperature>.*?)</td>.*?<td>(?P<wind>.*?)</td>', re.S)
+    matches = pattern.finditer(html)
+    for row in matches:
+        date_str = row.group('date').strip()
+        # 将日期字符串转换为日期格式
+        date = datetime.strptime(date_str, "%Y年%m月%d日")
+        weather = row['weather'].strip()
+        temperature = row['temperature'].strip()
+        wind = row['wind'].strip()
+        weather_info["history"].append(
+            {
+                "date": date.strftime("%Y-%m-%d"),
+                "weather": weather,
+                "temperature": temperature,
+                "wind": wind,
+            }
+        )
+    return weather_info if weather_info["history"] else None
+
+def main():
+    city_name = "kunming"
+    date_month = datetime.now() # 本月
+    weather_info = get_weather_history_monthly(city_name, date_month)
+    print(json.dumps(weather_info, ensure_ascii=False, indent=4))
+    return
+
+if __name__ == "__main__":
+    main()
--- 2024_暑假练习/编程应用练习/06_获取天气信息/WeatherInfo.py
+++ 2024_暑假练习/编程应用练习/06_获取天气信息/WeatherInfo.py
@@ -7,10 +7,11 @@
 # 后续用于数据统计、分析和绘制图表
 # 作者：loongba
 # 修改记录：
-#   Ver1.0 2024-08-26 调用 API 查询北京未来三天的天气信息，未读取参数、未加入缓存
-#   Ver1.1 TODO: 读取城市名字，调用 API 查询城市编码
+#   Ver1.0 20240826 调用 API 查询北京未来三天的天气信息，未读取参数、未加入缓存
+#   Ver1.1 20240827: 读取城市名字，调用 API 查询城市编码
 #   Ver1.2 TODO: 检查是否有缓存、读取缓存、缓存到本地文件
-#   Ver1.3 TODO: 加入数据统计和绘制图表
+#   Ver1.3 TODO: 加入模块：从某网站读取历史天气信息并保存为文件
+#   Ver1.x TODO: 加入数据统计和绘制图表
 
 # 自顶向下，逐层分解
 # 主要步骤：
@@ -23,31 +24,66 @@
 from datetime import datetime
 import requests
 import json
+import sys
+
+api_key = "4221343812994b3db7eb5cc3bb6e252f"
+
+# 从运行参数中获取城市名称或 ID
+def get_argv():
+    if len(sys.argv) > 1:
+        location = sys.argv[1]
+        if len(sys.argv) > 2:
+            location_adm = sys.argv[2]
+    else:
+        print("请提供有效的城市 ID 或 城市名称")
+        sys.exit(1)
+
+    return location, location_adm
+
+# 获取城市的地理信息：行政区划编码、城市名称
+def get_geo_info(location, location_adm=""):
+    api_url = f"https://geoapi.qweather.com/v2/city/lookup?location={location}&adm={location_adm}&range=cn&key={api_key}"
+    response = requests.get(api_url)
+    location = response.json()
+    print(api_url)
+    return {
+        "location_id": location["location"][0]["id"],
+        "location_name": location["location"][0]["name"],
+        "location_adm1": location["location"][0]["adm1"],
+        "location_adm2": location["location"][0]["adm2"],
+    }
+
 
 # 判断是否已经有本地数据缓存
 def is_cache_exists(location, date):
     # TODO: 判断是否已经有本地数据缓存
     return False
 
+
 # 从本地数据文件获取天气信息
 def get_weather_info_from_cache(location, date):
 
     return "晴，最高温度 30 度，最低温度 20 度"
+
+
 # 调用 API 获取天气信息，并缓存
-def get_weather_info_from_api(api_key, location, date = datetime.now().strftime("%Y-%m-%d")):
+def get_weather_info_from_api(location, date=datetime.now().strftime("%Y-%m-%d")):
     # 调用 和风天气的 API 获取未来三日天气预报
     url = f"https://devapi.qweather.com/v7/weather/3d?location={location}&key={api_key}"
-    # print(url)
     response = requests.get(url)
     weather_info = response.json()
-    # TODO: 将天气信息缓存到数据文件
-
-    return weather_info
+    if "code" in weather_info and weather_info["code"] == "200":
+        # TODO: 将天气信息缓存到数据文件
+        return weather_info
+    #print(url)
+    #print(weather_info)
+    return None
 
 # 将天气信息缓存到数据文件
 def save_weather_info_to_cache(location, weather_info, date):
-    #print(f"城市：{location} 从 {date} 起，未来三日天气预报：\r\n\t{weather_info}")
+    # print(f"城市：{location} 从 {date} 起，未来三日天气预报：\r\n\t{weather_info}")
     return
+
 
 # 显示天气信息
 def print_weather_info(location_name, weather_info):
@@ -60,26 +96,35 @@ def print_weather_info(location_name, weather_info):
         print(f"\t\t日升时间：{day_info['sunrise']}\t日落时间：{day_info['sunset']}")
     return
 
+
 def main():
-    api_key = "4221343812994b3db7eb5cc3bb6e252f"
     # 1. 获取参数：要查询哪个城市、哪一天的天气信息
-    location = "101010100" 
-    location_name = "北京"
+    location, location_adm = get_argv()
+    # 从运行参数中获取城市名称或 ID
+    location_info = get_geo_info(location, location_adm)
+    location_id = location_info["location_id"]
+    location_name = location_info["location_name"]
+    print(f'{location_info["location_adm1"]} {location_info["location_adm2"]} {location_info["location_name"]} {location_info["location_id"]} 未来三日天气预报：')
+
     # 默认获取今天的日期
     date = datetime.now()
     # 2. 判断是否已经有本地数据缓存
-    if is_cache_exists(location, date):
+    if is_cache_exists(location_id, date):
         # 2.1. 如果有缓存，从本地数据文件获取天气信息
-        weather_info = get_weather_info_from_cache(location, date)
-        #print(f"本地缓存中查询到天气信息")
+        weather_info = get_weather_info_from_cache(location_id, date)
+        # print(f"本地缓存中查询到天气信息")
     else:
         # 2.2. 如果没有缓存，则调用 API 获取天气信息，并将天气信息保存到数据文件
-        weather_info = get_weather_info_from_api(api_key, location, date)
-        #print(f"API 查询到天气信息")
-    # 3. 显示天气信息    
-    print_weather_info(location_name, weather_info)
+        weather_info = get_weather_info_from_api(location_id)
+        # print(f"API 查询到天气信息")
+    # 3. 显示天气信息
+    if weather_info:
+        print_weather_info(location_name, weather_info)
+    else:
+        print("查询天气信息失败")
 
     return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
