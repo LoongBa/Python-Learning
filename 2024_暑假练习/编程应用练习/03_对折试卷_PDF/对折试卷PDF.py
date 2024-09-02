de649f20ce65abdaad1d138b0f3d26d467daa66b
CoffeeScholar <76543969+LoongBa@users.noreply.github.com>
2024-09-03 01:13:34 +0800

    微调

--- 2024_暑假练习/编程应用练习/03_对折试卷_PDF/对折试卷PDF.py
+++ 2024_暑假练习/编程应用练习/03_对折试卷_PDF/对折试卷PDF.py
@@ -1,19 +1,21 @@
 # 学习中经常有各种试卷的 PDF 文件，其中一部分是 A3 幅面
 # A3 幅面打印时需要对应的 A3 打印机，一般只有打印店、办公室才有，家用一般是小一些的 A4 打印机
-# A3 幅面的尺寸是 A4 幅面的两倍。如果将一张 A3 幅面的试卷，对半拆分为两张 A4 幅面的试卷，就可以用家用打印机打印。
+# A3 幅面的尺寸是 A4 幅面的两倍。如果将一张 A3 幅面的试卷，对半对折为两张 A4 幅面的试卷，就可以用家用打印机打印。
 # 但有部分 A3 幅面试卷是分三段的，就不能简单的“对半分”。本工具，主要针对可以对半分的 A3 幅面试卷。
 
-# 拆分试卷 PDF 工具
-# 主要功能：将 A3 幅面的 PDF 试卷对半拆分为 A4 幅面的 PDF
+# 对折试卷 PDF 工具
+# 主要功能：将 A3 幅面的 PDF 试卷对半对折为 A4 幅面的 PDF
 # 作者：loonba
 # 版本号：Ver1.1
 
 
 # 主要步骤：
-# 1. 获得要拆分的 PDF 文件的名字和完整路径；
+# 1. 获得要对折的 PDF 文件的名字和完整路径；
 # 2. 将 PDF 文件中的每一页提取为图像对象；
-# 3. 对提取的每一页的图像对象，左右拆分为两个图像对象（各为原来图像的一半）
-# 4. 按顺序将拆分后的图像对象重新组成一个 PDF 文件，保存原PDF文件相同的位置，文件名加后缀 "_A4"
+# 3. 对提取的每一页的图像对象，左右对折为两个图像对象
+#       （各为原来图像的一半）
+# 4. 按顺序将对折后的图像对象重新组成一个 PDF 文件，
+#       保存原PDF文件相同的位置，文件名加后缀 "_A4"
 # 5. 输出提示信息、打开文件
 
 # V1.0 完成基本功能
@@ -22,70 +24,20 @@
 
 import sys
 import os
+from PIL import Image
 from pdf2image import convert_from_path
 # 添加上级目录到 sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from myUtils import print_error, run_file_by_default_app, print_color
 
-# 拆分指定的 PDF 文件为多个图片文件
-def split_pdf_into_images(pdf_file_path: str) -> str:
-    # 每个函数，是个承担“单一职责”的命令集合
-    # 就像应用题需要提供必要的数据，这里就对应着参数；
-    # 解应用题时，需要根据题目（参数）提供的直接信息推导出所需要的间接信息
-    if not os.path.isfile(pdf_file_path) or not pdf_file_path.lower().endswith(".pdf"):
-        return False, f"请提供有效的 PDF 文件：{pdf_file_path}"
-
-    # 调用第三方包，拆分 PDF 为多个图像的对象
-    pages_images = convert_from_path(pdf_file_path)  # convert 转换
-    pdf_file_name = os.path.basename(pdf_file_path).lower()
-    print_color(f"\t正在拆分：{pdf_file_name}")  # 输出信息
-
-    image_list = []
-    # 遍历每一页对应的图像文件（内存中）
-    for i, page_image in enumerate(pages_images):
-        # 利用了 enumerate 将可枚举类型返回为 加了索引的 可枚举类型：元组的 Array
-        page_number = i + 1
-        # 拆分页对应的图片为两个图片
-        page_images_list = split_image_into_images(
-            page_image
-        )  # 调用函数拆分图片
-        image_list.extend(page_images_list) # extend 扩展， append 追加， Injection 注入
-        print_color(f"\t\t成功拆分第 {page_number} 页。")  # 输出信息
-
-    return True, image_list
-
-
-# 将指定的图片文件，拆分成两个图片文件
-def split_image_into_images(page_image: any):   # TODO: 将来扩展为支持拆分为三部分，支持剪裁试卷周围区域
-    # 获得图像的宽度和高度
-    width, height = page_image.size
-    new_width = width // 2
-    image_left = page_image.crop((0, 0, new_width, height))
-    image_right = page_image.crop((new_width, 0, width, height))
-
-    image_list = [image_left, image_right]
-    return image_list
-
-def merge_images_into_pdf(images_list: str, new_pdf_file_path: str) -> str:
-    pdf = images_list[0]
-    pdf.save(
-        new_pdf_file_path,
-        "PDF",
-        resolution = 100.0,
-        save_all = True,
-        append_images = images_list[1:],
-    )
-    return True, new_pdf_file_path  # 返回完整路径
-
-def main():
-    # 1. 获得要拆分的 PDF 文件的名字和完整路径；
+# 获得要对折的 PDF 文件的名字和完整路径
+def get_pdf_file_path() -> str:
     if len(sys.argv) < 2:
-        print_error("请提供要拆分的 PDF 文件的完整路径。")
-        return
+        print_error("请提供要对折的 PDF 文件的完整路径。")
+        return None, None
 
     pdf_file_path = sys.argv[1]
     if not os.path.isabs(pdf_file_path):    # 是否是绝对路径
-        #current_workingfolder_path = os.path.abspath(os.curdir)    # 注意：os.curdir 获取到的可能不是绝对路径，需要加上保险：os.path.abspath()
         #pdf_file_path = os.path.join(current_workingfolder_path, pdf_file_path)    
         pdf_file_path = os.path.abspath(pdf_file_path)      # os.path.abspath() 获取绝对路径——拼接了当前工作目录的绝对路径
         # 尝试基于当前工作目录查找文件
@@ -96,22 +48,88 @@ def main():
         
     if not is_pdf(pdf_file_path):
         print_error(f"请提供有效的 PDF 文件：{pdf_file_path}")
-        return
+        return None, None
 
-    new_pdf_file_path = pdf_file_path.lower().replace(".pdf", "_A4.pdf")
-    # 2. 将 PDF 文件中的每一页的图片导出到临时目录，注意文件名的顺序；
-    is_success, image_list = split_pdf_into_images(pdf_file_path)
+    new_pdf_file_path = pdf_file_path.lower().replace(".pdf", "_A4.pdf")    
+    return pdf_file_path, new_pdf_file_path
+
+# 提取 PDF 文件中的每一页为图像对象
+def extract_pdf_into_images(pdf_file_path: str) -> str:
+    # 每个函数，是个承担“单一职责”的命令集合
+    # 就像应用题需要提供必要的数据，这里就对应着参数；
+    # 解应用题时，需要根据题目（参数）提供的直接信息推导出所需要的间接信息
+    if not is_pdf(pdf_file_path):
+        return False, f"请提供有效的 PDF 文件：{pdf_file_path}"
+
+    # 调用第三方包，对折 PDF 为多个图像的对象
+    pages_images = convert_from_path(pdf_file_path)  # convert 转换
+    pdf_file_name = os.path.basename(pdf_file_path).lower()
+    print_color(f"\t正在对折：{pdf_file_name}")  # 输出信息
+
+    image_list = []
+    # 遍历每一页对应的图像文件（内存中）
+    for i, page_image in enumerate(pages_images):
+        # 利用了 enumerate 将可枚举类型返回为 加了索引的 可枚举类型：元组的 Array
+        page_number = i + 1
+        # 对折页对应的图片为两个图片
+        page_images_list = split_image_into_images(
+            page_image
+        )  # 调用函数对折图片
+        image_list.extend(page_images_list) 
+        # 顺手学单词：extend 扩展， append 追加， Injection 注入
+        print_color(f"\t\t成功对折第 {page_number} 页。")  # 输出信息
+
+    return True, image_list
+
+
+# 将指定的图片文件，拆分成两个图片文件
+# TODO: 将来扩展为支持对折为三部分、支持剪裁试卷周围区域
+def split_image_into_images(page_image: Image.Image) -> list[Image.Image]: 
+    # 获得图像的宽度和高度
+    width, height = page_image.size
+    new_width = width // 2
+    # 裁剪图片，将图片分成两部分
+    image_left = page_image.crop((0, 0, new_width, height))
+    image_right = page_image.crop((new_width, 0, width, height))
+
+    # 将裁剪后的图片存入列表
+    image_list = [image_left, image_right]
+    return image_list   # 返回图像列表，以便调用代码合并到已有数组
+
+# 合并图像列表为一个 PDF 文件
+def merge_images_into_pdf(images_list: list[Image.Image],
+                            new_pdf_file_path: str) -> str:
+    try:
+        pdf_first_page_image = images_list[0]
+        pdf_first_page_image.save(
+            new_pdf_file_path, "PDF", save_all = True,
+            # 追加剩余图像  append: 追加
+            append_images = images_list[1:], 
+        )
+        return True, ""
+    except Exception as e:
+        return False, f"合并图像列表为 PDF 文件失败：{e}"
+
+def main():
+    # 1. 获得要对折的 PDF 文件的名字和完整路径；
+    pdf_file_path, new_pdf_file_path = get_pdf_file_path()
+    if pdf_file_path is None:
+        return
+    
+    # 2. 提取 PDF 文件中的每一页为图像对象
+    is_success, image_list = extract_pdf_into_images(pdf_file_path)
     if not is_success:
-        print_error(f"拆分 PDF 文件失败：{image_list}")
+        print_error(f"提取 PDF 文件失败：{image_list}")
         return
-    # split 拆分     image 图像
 
-    # 3. 对临时目录中的所有拆分过的图片文件，按顺序重新组成一个 PDF 文件；
-    result = merge_images_into_pdf(
+    # 3. 对处理过的图像对象，按顺序重新组成PDF文件
+    result, error_message = merge_images_into_pdf(
         image_list, new_pdf_file_path
     )  # merge 融合、合并
     if result:
         print_color(f"PDF 文件已保存到: {new_pdf_file_path}")
+    else:
+        print_error(error_message)
 
     # 4. 运行新的 PDF 文件：使用默认的 PDF 阅读器打开
     run_file_by_default_app(new_pdf_file_path)
